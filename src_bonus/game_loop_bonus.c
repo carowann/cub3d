530fbf111a0d3ccade8d2429021f6db060280d84
carowann <cwannhed@student.42firenze.it>
2026-02-04 17:37:57 +0100

    moved data init

--- src_bonus/game_loop_bonus.c
+++ src_bonus/game_loop_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   game_loop_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: giomastr <giomastr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cwannhed <cwannhed@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/02 11:42:37 by cwannhed          #+#    #+#             */
-/*   Updated: 2026/02/03 16:48:33 by giomastr         ###   ########.fr       */
+/*   Updated: 2026/02/04 17:48:35 by cwannhed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	render_frame(t_data *data)
 	handle_keyboard_input(data);
 	raycasting(data);
 	draw_minimap(data);
-	mlx_put_image_to_window(data->mlx->mlx,
+	mlx_put_image_to_window(data->mlx->mlx,-pi
 		data->mlx->win,
 		data->mlx->img, 0, 0);
 	return (0);
